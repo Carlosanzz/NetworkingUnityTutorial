1b0ba66ccd335d351335203cb56740c1a813da27
Carlos Sanz Salcedo <carlosanz94@gmail.com>
2021-01-31 23:24:17 +0100

    Set-up of AWS dedicated server and windows/linux builds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,11 @@
-﻿using Mirror;
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
-    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    [SyncVar(hook = nameof(OnHolaCountChanged))] //SYNCRONIZED VARIABLE, OBJECT IN SERVER HAS AUTHORITY AND CALLS HOOK ON CLIENTS WHEN CHANGED
     int holaCount = 0;
     void HandleMovement()
     {
@@ -32,23 +32,23 @@ public class Player : NetworkBehaviour
     }
 
     [Command]
-    void CmdHola() {
+    void CmdHola() { // COMMAND CALLED IN CLIENT OBJECT AND EXECUTED IN SERVER OBJECT.
         Debug.Log("Received Hola from Client!");
         holaCount++;
         TargetReplyHola();
     }
 
-    [ClientRpc]
-    void RpcTooHigh() {
+    [ClientRpc]  
+    void RpcTooHigh() { // REMOTE PROCEDURE CALL CALLED IN SERVER OBJECT AND EXECUTED IN ALL CLIENTS OBJECT (CAN BE MODIFIED WITH NETWORK VISIBILITY).
         Debug.Log("Your client is too high!");
     }
 
     [TargetRpc]
-    void TargetReplyHola(){
+    void TargetReplyHola(){ // TARGET REMOTE PROCEDURE CALL, CALLED IN SERVER OBJECT AND EXECUTED IN SPECIFIC CLIENT OBJECT (CAN BE IMPLICIT LIKE HERE, BUT CAN EXPLICITLY SAY CLIENT CONNECTION)
         Debug.Log("Received Hola from Server!");
     }
 
-    void OnHolaCountChanged(int oldCount, int newCount) {
+    void OnHolaCountChanged(int oldCount, int newCount) { // CALLBACK N CHANGE 
         Debug.Log($"Player with NetID {netId} had sent {oldCount} holas, but now he has sent {newCount} holas.");
     }
 
---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2019.4.18f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\carlo\UnityProjects\0_2021GAMES\MultiplayerTemplate" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
