0b327d1596a4cbcd224c40ba4933544c5ccdf190
Carlos Sanz Salcedo <carlosanz94@gmail.com>
2021-01-31 18:54:28 +0100

    data sync tutorial

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    public override void OnStartServer() // GENERAL MIRROR CALLBACKS EXECUTED IN SERVER
+    {
+        Debug.Log("Server started!");
+    }
+
+    public override void OnStopServer() // GENERAL MIRROR CALLBACKS EXECUTED IN SERVER
+    {
+        Debug.Log("Server stopped!");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn) // GENERAL MIRROR CALLBACKS EXECUTED IN CLIENT
+    {
+        Debug.Log("Connected to server!");
+    }
+    public override void OnClientDisconnect(NetworkConnection conn) // GENERAL MIRROR CALLBACKS EXECUTED IN CLIENT
+    {
+        Debug.Log("Disconnected to server!");
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
-
+    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    int holaCount = 0;
     void HandleMovement()
     {
         if (isLocalPlayer)
@@ -15,10 +16,40 @@ public class Player : NetworkBehaviour
             Vector3 movement = new Vector3(moveHorizontal*0.1f, moveVertical*0.1f, 0);
             transform.position += movement;
         }
+
     }
 
     private void Update() {
         HandleMovement();
+        if(isLocalPlayer && Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Sending Hola to Server!");
+            CmdHola();
+        }
+
+        if(isServer && transform.position.y > 10){ // Bad practice 
+            RpcTooHigh();
+        }
+    }
+
+    [Command]
+    void CmdHola() {
+        Debug.Log("Received Hola from Client!");
+        holaCount++;
+        TargetReplyHola();
+    }
+
+    [ClientRpc]
+    void RpcTooHigh() {
+        Debug.Log("Your client is too high!");
+    }
+
+    [TargetRpc]
+    void TargetReplyHola(){
+        Debug.Log("Received Hola from Server!");
+    }
+
+    void OnHolaCountChanged(int oldCount, int newCount) {
+        Debug.Log($"Player with NetID {netId} had sent {oldCount} holas, but now he has sent {newCount} holas.");
     }
 
 }
