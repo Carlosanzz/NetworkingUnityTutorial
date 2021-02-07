d4a4d8c14eb81906497da5f6db619e5f3001a009
Carlos Sanz Salcedo <carlosanz94@gmail.com>
2021-02-07 15:30:18 +0100

    little scripts update

---
+++ Assets/Scripts/MyPlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyPlayerManager : NetworkBehaviour
+{
+
+    public GameObject CheckersBoard;
+
+    public Piece[,] pieces = new Piece[8,8];
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        CheckersBoard = GameObject.Find("CheckersBoard");
+    }
+
+    [Server]
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
